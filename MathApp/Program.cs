4dbacc50b61888d69ccc0899aa5a44002906f541
Ciano <clarkianbautista5@gmail.com>
2026-02-07 13:16:50 +0800

    Add files via upload

---
+++ MathApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathApp
+{
+    internal class MathProgram
+    {
+        static void Main(string[] args)
+        {
+            double a = 5.0;
+            double b = 14.0;
+            double c = -15.0;
+
+            double powerR = Math.Pow(a, b);
+            double sqrtR = Math.Sqrt(a);
+            double logR = Math.Log(a);
+            double expR = Math.Exp(a);
+            double absR = Math.Abs(c);
+
+            Console.WriteLine($"{a} raised to the power of {b} is {powerR}");
+            Console.WriteLine($"The square root of {a} is {sqrtR}");
+            Console.WriteLine($"The natural logarithm of {a} is {logR}");
+            Console.WriteLine($"The exponential of {a} is {expR}");
+            Console.WriteLine($"The absolute value of {c} is {absR}");
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MathApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MathApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MathApp")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("acf66e22-a5e3-468a-847d-7be83a5feb6b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
